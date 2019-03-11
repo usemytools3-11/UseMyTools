f19062c9b04fabb44c8a12d8e4a049d4d161e66f
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-11 12:43:28 -0400

    added login page default

--- usemytools/src/routes/index.js
+++ usemytools/src/routes/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import { connect } from 'react-redux';
 import HomePage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
 
 const Routes = (props) => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact render={() => <HomePage />}/>
+                <Route path="/login" exact render={() => <LoginPage />}/>
             </Switch>
         </BrowserRouter>
     );
