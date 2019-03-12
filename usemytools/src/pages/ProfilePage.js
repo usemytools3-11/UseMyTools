3715cad5d3f900b6552d62346ee59fe3d50704bf
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-11 23:41:50 -0400

    added user items on profile page

--- usemytools/src/pages/ProfilePage.js
+++ usemytools/src/pages/ProfilePage.js
@@ -15,6 +15,11 @@ class ToolsPage extends Component {
                 <h1>Profile page</h1>
                 <h2>{this.props.first_name} {this.props.last_name}</h2>
                 <p>{this.props.email}</p>
+
+                <h1>Your items:</h1>
+                {this.props.tools.map(elem =><p key={elem.id}>{elem.name}</p>)}
+
+                <h1>Items you borrowed:</h1>
             </>
         );
     }
@@ -24,7 +29,9 @@ const mapStateToProps = state => {
     return {
         first_name: state.auth.user ? state.auth.user.first_name : '',
         last_name: state.auth.user ? state.auth.user.last_name : '',
-        email: state.auth.user ? state.auth.user.email : ''
+        email: state.auth.user ? state.auth.user.email : '',
+        userID: state.auth.user ? state.auth.user.id : -1,
+        tools: state.items.tools ? state.items.tools.filter(elem => elem.id === state.auth.user.id) : []
     }
 }
 
