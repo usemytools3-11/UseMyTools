f5cffaf56cb6a7ad9871a34210775a9158791b2f
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-11 23:14:09 -0400

    updated tools page

--- usemytools/src/pages/ToolsPage.js
+++ usemytools/src/pages/ToolsPage.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Tools from '../containers/ToolsContainer';
 import { connect } from 'react-redux';
-import { fetchTools } from '../actions';
+import { fetchTools, fetchTool } from '../actions';
 
 class ToolsPage extends Component {
     componentDidMount() {
-        if(this.props.tools.length === 0) this.props.fetchTools();
+        if(this.props.tools.length === 0){
+            this.props.fetchTools();
+        }
     }
 
     render() {
+        console.log(this.props.tool);
         return (
             <>
                 <h1>Tools page</h1>
@@ -21,12 +24,14 @@ class ToolsPage extends Component {
 const mapStateToProps = state => {
     return {
         authenticated: state.auth.authenticated || false,
-        tools: state.items.tools || []
+        tools: state.items.tools || [],
+        tool: state.items.tool || null
     }
 }
 
 const mapDispatchToProps = {
     fetchTools,
+    fetchTool
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ToolsPage);
