561a3ea4b5c64bba93bc57fd0a83de38d7e0d7b0
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-11 23:14:30 -0400

    added items actions to fetch all tools and one tool

--- usemytools/src/actions/items.js
+++ usemytools/src/actions/items.js
@@ -1,3 +1,91 @@
 import {
-    // ERROR
+    // ERROR,
+    ITEM_FETCH_REQUEST,
+    ITEM_FETCH_SUCCESS,
+    ITEM_FETCH_FAILURE,
+    ITEMS_FETCH_REQUEST,
+    ITEMS_FETCH_SUCCESS,
+    ITEMS_FETCH_FAILURE
 } from '../constants/actionTypes';
+import axios from 'axios';
+import { API_URL } from '../constants/config';
+
+export function fetchTools(){
+    return dispatch => {
+        dispatch(requestFetch());
+        
+        axios
+            .get(API_URL+'/tools/')
+            .then(res => {
+                if(res.status===200){
+                    dispatch(receiveFetch(res.data));
+                }else{
+                    dispatch(errorFetch(res.data.error));
+                    return Promise.reject(res.data);
+                }
+            })
+            .catch(err => console.log(err));
+        
+    }
+
+
+    function requestFetch(){
+        return {
+            type: ITEMS_FETCH_REQUEST
+        }
+    }
+
+    function receiveFetch(items){
+        return {
+            type: ITEMS_FETCH_SUCCESS,
+            payload: items
+        }
+    }
+
+    function errorFetch(err){
+        return {
+            type: ITEMS_FETCH_FAILURE,
+            payload: err
+        }
+    }
+}
+
+export function fetchTool(id){
+    return dispatch => {
+        dispatch(requestFetch());
+        
+        axios
+            .get(API_URL+`/tools/${id}`)
+            .then(res => {
+                if(res.status===200){
+                    dispatch(receiveFetch(res.data));
+                }else{
+                    dispatch(errorFetch(res.data.error));
+                    return Promise.reject(res.data);
+                }
+            })
+            .catch(err => console.log(err));
+        
+    }
+
+
+    function requestFetch(){
+        return {
+            type: ITEM_FETCH_REQUEST
+        }
+    }
+
+    function receiveFetch(item){
+        return {
+            type: ITEM_FETCH_SUCCESS,
+            payload: item
+        }
+    }
+
+    function errorFetch(err){
+        return {
+            type: ITEM_FETCH_FAILURE,
+            payload: err
+        }
+    }
+}
