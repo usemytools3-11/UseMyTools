ed0fe37fda4a9dd4c75bcf7749d6a61c39acb1c9
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-14 15:29:27 -0400

    updated navbar, added bootstrap

--- usemytools/src/components/Navbar.js
+++ usemytools/src/components/Navbar.js
@@ -1,27 +1,126 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { logoutUser } from '../actions';
 
-const Navbar = (props) => {
-    return (
-        <>
-            {!props.authenticated &&
-            <>
-                <Link to="/">HOME</Link><br/>
-                <Link to="/login">LOGIN</Link><br/>
-                <Link to="/register">REGISTER</Link>
-            </>}
-            {props.authenticated &&
-            <>
-                <Link to="/tools">TOOLS_TO_RENT</Link><br/>
-                <Link to="/profile">PROFILE</Link><br/>
-                <Link to="/profile/tools">YOUR_TOOLS</Link><br/>
-                <Link to="/profile/tools/new">NEW_TOOL</Link><br/>
-                <button onClick={props.logoutUser}>LOGOUT</button>
-            </>}
-        </>
-    );
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+
+import styled from 'styled-components';
+
+const NavbarBrandStyled = styled(NavbarBrand)`
+    font-size: 24px !important;
+`;
+
+const NavbarStyled = styled(Navbar)`
+    margin-bottom: 5rem;
+    border-bottom: 1px solid lightgray;
+`;
+
+
+// const NavbarInput = styled.input`
+//     width: 200px;
+//     text-align: center;
+//     box-sizing: border-box;
+//     background: url(https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_search_48px-128.png);
+//     background-color: rgb(250, 250, 250);
+//     background-size: 16px;
+//     background-repeat: no-repeat;
+//     background-position: 54px 50%;
+//     &:focus {
+//         background-position: 0;
+//     }
+// `;
+
+class NavbarComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false
+        }
+
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+            ...this.state,
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        const navlinksPublic = [
+            {
+                linkTo: '/login',
+                text: 'Log in'
+            },
+            {
+                linkTo: '/register',
+                text: 'Register'
+            }
+        ];
+
+        const navlinksPrivate = [
+            {
+                linkTo: '/tools',
+                text: 'Tools to rent'
+            },
+            {
+                linkTo: '/profile',
+                text: 'Your profile'
+            },
+            {
+                linkTo: '/profile/tools',
+                text: 'Your tools'
+            },
+            {
+                linkTo: '/profile/tools/new',
+                text: 'Add new tool'
+            }
+        ];
+
+        return (
+            <NavbarStyled color="white" light expand="md">
+                <NavbarBrandStyled href="/">
+                    UseMyTools
+                </NavbarBrandStyled>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        {!this.props.authenticated && navlinksPublic.map((elem, i) =>
+                            <NavItem key={i}>
+                                <NavLink href={elem.linkTo}>
+                                    {elem.text}
+                                </NavLink>
+                            </NavItem>
+                        )}
+                        {this.props.authenticated && navlinksPrivate.map((elem, i) =>
+                            <NavItem key={i}>
+                                <NavLink href={elem.linkTo}>
+                                    {elem.text}
+                                </NavLink>
+                            </NavItem>
+                        )}
+                        {this.props.authenticated &&
+                            <NavItem>
+                                <NavLink hre='/' onClick={this.props.logoutUser}>
+                                    Log out
+                                </NavLink>
+                            </NavItem>
+                        }
+                    </Nav>
+                </Collapse>
+            </NavbarStyled>
+        );
+    }
 }
 
 const mapStateToProps = state => {
@@ -31,8 +130,8 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = {
-    logoutUser,
+    logoutUser
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
+export default connect(mapStateToProps, mapDispatchToProps)(NavbarComponent);
--- usemytools/src/index.js
+++ usemytools/src/index.js
@@ -4,6 +4,7 @@ import Routes from './routes';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import store from './store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import createBrowserHistory from 'history/createBrowserHistory';
 
